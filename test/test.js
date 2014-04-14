4dfe121981c7146dff69398a83de774fbf98cfa6
Leo Koppelkamm <hello@leo-koppelkamm.de>
2014-04-14 16:45:44 +0200

    Rework to be closer to what Yaml Frontmatter would generate
    Only properties inside the first --- block are parsed. Properties after the first block are treated as normal content. This breaks constructs like:
    ---
    title: A
    ---
    option: B
    ---
    
    I think this is much saner as it allows to use colons and dashes in the content freely without worrying that some parts of the content are unexpectedly treated as properties.

--- test/test.js
+++ test/test.js
@@ -14,28 +14,34 @@ function parseFile(file) {
 
 describe('Properties', function() {
 	it('should convert lines starting with word+: to properties', function() {
-		var param = parsedown('test:test test');
+		var param = parsedown('---\ntest:test test');
 		expect(param.test).to.equal('test test');
-		expect(param.content).to.be.undefined;
+		expect(param.content).equal('');
 	});
 
 	it('should put all text into content property if there are no properties present', function() {
 		var param = parsedown('test test');
 		expect(param.content).to.equal('test test');
+
+		param = parsedown('test test\nmoreLines');
+		expect(param.content).to.equal('test test\nmoreLines');
+
+		param = parsedown('test test\nmoreLines\n--');
+		expect(param.content).to.equal('test test\nmoreLines\n--');
 	});
 
 	it('should preserve linebreaks', function() {
-		var param = parsedown('test:test test\ntest');
+		var param = parsedown('---\ntest:test test\ntest');
 		expect(param.test).to.equal('test test\ntest');
 	});
 
 	it('should remove trailing linebreaks', function() {
-		var param = parsedown('test: test test\n');
+		var param = parsedown('---\ntest: test test\n');
 		expect(param.test).to.equal('test test');
 	});
 
 	it('should preserve double linebreaks', function() {
-		var param = parsedown('test:test test\n\ntest');
+		var param = parsedown('---\ntest:test test\n\ntest');
 		expect(param.test).to.equal('test test\n\ntest');
 	});
 
@@ -46,24 +52,18 @@ describe('Properties', function() {
 	});
 
 	it('should allow numbers in property keys', function() {
-		var param = parsedown('test123:test');
+		var param = parsedown('---\ntest123:test');
 		expect(param.test123).to.equal('test');
 	});
 
 	it('should allow underscores in property keys', function() {
-		var param = parsedown('test_123:test');
+		var param = parsedown('---\ntest_123:test');
 		expect(param.test_123).to.equal('test');
 	});
 
 	it('should allow capital letters in property keys', function() {
-		var param = parsedown('camelCase:test');
+		var param = parsedown('---\ncamelCase:test');
 		expect(param.camelCase).to.equal('test');
-	});
-
-	it('should remove --- dividers', function() {
-		var param = parsedown('first: value1\n---\nsecond:value2');
-		expect(param.first).to.equal('value1');
-		expect(param.second).to.equal('value2');
 	});
 
 	it('should swallow line breaks after --- dividers', function() {
@@ -77,33 +77,43 @@ describe('Properties', function() {
 	});
 
 	it('should swallow line breaks directly after a property name', function() {
-		var param = parsedown('test:\ntest');
+		var param = parsedown('---\ntest:\ntest');
 		expect(param.test).to.equal('test');
 	});
 
 	it('should swallow spaces directly after a property name', function() {
-		var param = parsedown('test: testing');
+		var param = parsedown('---\ntest: testing');
 		expect(param.test).to.equal('testing');
 
-		param = parsedown('test:       testing');
+		param = parsedown('---\ntest:       testing');
 		expect(param.test).to.equal('testing');
 	});
 
 	it('should swallow line-breaks & spaces directly after a property name', function() {
-		var param = parsedown('test:       \n\ntesting');
+		var param = parsedown('---\ntest:       \n\ntesting');
 		expect(param.test).to.equal('testing');
 	});
 
 	it('should ignore spaces in property keys', function() {
-		var param = parsedown(' test:test');
+		var param = parsedown('---\n test:test\nsecond:value');
 		expect(param['test']).to.be.undefined;
-		expect(param.content).to.equal(' test:test');
+		expect(param['second']).to.equal('value');
 	});
 
-
 	it('lastKey should be nullified after --- divider, leading to next content without a property name to be placed inside content', function() {
-		var param = parsedown('test:test\n---\nblabla');
+		var param = parsedown('---\ntest:test\n---\nblabla');
 		expect(param['test']).to.equal('test');
 		expect(param.content).to.equal('blabla');
 	});
+
+	it('should only parse properties in the first --- block', function() {
+		var param = parsedown('---\nfirst: value1\n---\nsecond:value2');
+		expect(param.first).to.equal('value1');
+		expect(param.content).to.equal('second:value2');
+
+		param = parsedown('---\nfirst: value1\n---\nsecond:value2\n---\nsomemore');
+		expect(param.first).to.equal('value1');
+		expect(param.content).to.equal('second:value2\n---\nsomemore');
+	});
+	
 });
